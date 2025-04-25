fa8f420d0e4b3b10aa4b28b0885e7835a5770f3c
haahmed01 <dzahme33@gmail.com>
2025-04-25 15:01:05 +0100

    edit

--- tpBigdata_7/producer_iot.py
+++ tpBigdata_7/producer_iot.py
@@ -2,12 +2,15 @@ from kafka import KafkaProducer
 import time
 import random
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+# اتصل ببروكر Kafka على 127.0.0.1:9092
+producer = KafkaProducer(bootstrap_servers=['127.0.0.1:9092'])
 
 while True:
+    # محاكاة قراءة درجة حرارة
     temperature = random.uniform(30.0, 100.0)
     message = f"Temperature: {temperature:.2f}°C"
-    producer.send('iot-topic', message.encode('utf-8'))
-    producer.flush()
-    print(f"Sent: {message}")
+    # إرسال الرسالة للـ topic
+    producer.send('iot-test', message.encode('utf-8'))
+    producer.flush()  # لضمان الإرسال الفوري
+    print("Sent:", message)
     time.sleep(2)
